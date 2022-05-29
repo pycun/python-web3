82d63cb6cd3984a4bb9b27f5f7c86185403a3dec
Victor <victor@wisphub.net>
2022-05-29 12:48:02 -0500

    add config file

--- smartcontracts/settings.py
+++ smartcontracts/settings.py
@@ -10,20 +10,32 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Get the smartcontracts_ENVIRON value to load the env variables
+server_env = os.environ.get('SMARTCONTRACTS_ENVIRON') or 'development'
+path = str(BASE_DIR / F'.config/environment/{server_env}/.env')
+environ.Env.read_env(path)
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, True),
+    SECRET_KEY=(str, ),
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=3!@xu&2ld!r-3ew6-g)xhvo@9lrel+yuh(6#af8^$9q8u1gb@'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
